1bc097630a4a6fb589141a06275d15d0a3e8621c
nihat2359-prog <nihat2359@gmail.com>
2026-04-15 13:06:59 +0300

    fix

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,4 @@
 import Flutter
-import FirebaseCore
 import UIKit
 
 @main
@@ -8,7 +7,6 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    FirebaseApp.configure()
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 
